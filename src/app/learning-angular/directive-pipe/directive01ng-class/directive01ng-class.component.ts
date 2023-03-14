f9d769b08464735c50f1fa9b3d9b607b452c76da
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2023-03-14 22:08:11 +0200

    build on git

--- src/app/learning-angular/directive-pipe/directive01ng-class/directive01ng-class.component.ts
+++ src/app/learning-angular/directive-pipe/directive01ng-class/directive01ng-class.component.ts
@@ -3,15 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directive01ng-class',
   templateUrl: './directive01ng-class.component.html',
-  styleUrls: ['./directive01ng-class.component.css']
+  styleUrls: ['./directive01ng-class.component.css'],
 })
 export class Directive01NgClassComponent implements OnInit {
-
   flag = true;
 
-  
-  constructor() { }
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 }
